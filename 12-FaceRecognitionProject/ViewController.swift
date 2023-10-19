ac10fd9302683610ae180cf33446ea1138038be7
Berke Ersiz <berkrsz@gmail.com>
2023-10-19 18:52:43 +0300

    completed

---
+++ 12-FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  12-FaceRecognitionProject
+//
+//  Created by Berke Ersiz on 23.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+        
+    }
+    
+}
+
